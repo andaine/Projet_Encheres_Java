872b9aedd7a17c11dba9ee01ae6467d87fe8f316
tandaine2022 <tandaine2022@16SE44-CXH3P02.ad.campus-eni.fr>
2023-01-30 16:43:15 +0100

    FIX ServletCreateAccount url

--- src/main/java/fr/eni/ecole/enchere/servlet/ServletCreateAccount.java
+++ src/main/java/fr/eni/ecole/enchere/servlet/ServletCreateAccount.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ServletCreateAccount
  */
-@WebServlet("/CreateAccount")
+@WebServlet("/ServletCreateAccount")
 public class ServletCreateAccount extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
