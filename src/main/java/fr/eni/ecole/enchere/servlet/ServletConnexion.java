1c578642aee0eb7fe5db45e9f283bfc8358f1c36
twillem2022 <twillem2022@16SE44-HRH3P02.ad.campus-eni.fr>
2023-01-30 16:59:38 +0100

    ADD servlet userConnecte

---
+++ src/main/java/fr/eni/ecole/enchere/servlet/ServletConnexion.java
@@ -0,0 +1,46 @@
+package fr.eni.ecole.enchere.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletConnexion
+ */
+@WebServlet("/ServletConnexion")
+public class ServletConnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("doget - servlet accueil");
+		request.setCharacterEncoding("UTF-8");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/userConnecte.jsp");
+		rd.forward(request, response);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
