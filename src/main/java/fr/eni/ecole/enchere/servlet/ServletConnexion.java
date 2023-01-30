f55c8dc09535168b19a1c7a3a1eb3d3714edf146
twillem2022 <twillem2022@16SE44-HRH3P02.ad.campus-eni.fr>
2023-01-30 17:11:57 +0100

    FIX edit servlet connexion

--- src/main/java/fr/eni/ecole/enchere/servlet/ServletConnexion.java
+++ src/main/java/fr/eni/ecole/enchere/servlet/ServletConnexion.java
@@ -20,8 +20,11 @@ public class ServletConnexion extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doget - servlet accueil");
+		request.setCharacterEncoding("UTF-8");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/userConnecte.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
