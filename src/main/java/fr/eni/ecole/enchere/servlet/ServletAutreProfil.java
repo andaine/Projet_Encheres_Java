a1c90f014de7f8a729e28440312aaaf57e7d5708
twillem2022 <twillem2022@16SE44-HRH3P02.ad.campus-eni.fr>
2023-01-31 12:14:18 +0100

    add servlet profil jeanMichel

---
+++ src/main/java/fr/eni/ecole/enchere/servlet/ServletAutreProfil.java
@@ -0,0 +1,27 @@
+package fr.eni.ecole.enchere.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletAutreProfil
+ */
+@WebServlet("/ServletAutreProfil")
+public class ServletAutreProfil extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/autreProfil.jsp");
+		rd.forward(request, response);
+	}
+
+}
