ddb67811210daf45653cb0ff81dc6b356e78d1cd
tandaine2022 <tandaine2022@16SE44-CXH3P02.ad.campus-eni.fr>
2023-02-01 14:09:33 +0100

    ADD ArticleDAOJdbcImpl : méthode select_categories

--- src/main/java/fr/eni/ecole/enchere/dal/ArticleDAOJdbcImpl.java
+++ src/main/java/fr/eni/ecole/enchere/dal/ArticleDAOJdbcImpl.java
@@ -1,5 +1,32 @@
 package fr.eni.ecole.enchere.dal;
 
-public class ArticleDAOJdbcImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import fr.eni.ecole.enchere.bo.Utilisateur;
+import fr.eni.ecole.enchere.exception.BusinessException;
+
+public class ArticleDAOJdbcImpl implements ArticleDAO{
+
+	private static final String SELECT_CATEGORIES ="SELECT * FROM Articles";
+
+	@Override
+	public void selectCategories() throws BusinessException {
+
+		try (Connection cnx = ConnectionProvider.getConnection()) {
+
+			Statement pstmt = cnx.createStatement();
+
+			pstmt.executeQuery(SELECT_CATEGORIES);
+
+		} catch (SQLException e) {
+			BusinessException be = new BusinessException();
+			be.addMessage("la liste des catégories n'a pas pu être chargée");
+			// throw lance l'exception et envoie le message aux couches supérieures
+			throw be;
+		}
+	}
 }
