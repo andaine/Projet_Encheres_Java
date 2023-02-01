5c5afa9e77872a5ca6f209a240baf3e2c2ce83fc
gbaudouin2022 <gbaudouin2022@16SE44-HQH3P02.ad.campus-eni.fr>
2023-02-01 14:19:41 +0100

    Add constructeur utilisateurs

--- src/main/java/fr/eni/ecole/enchere/bo/Utilisateur.java
+++ src/main/java/fr/eni/ecole/enchere/bo/Utilisateur.java
@@ -163,6 +163,21 @@ public class Utilisateur {
 		this.listArticleVendus = listArticleVendus;
 	}
 	
+	
+	
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			String codePostal, String ville) {
+		super();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+
 	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone,
 			String rue, String codePostal, String ville, String motDePasse) {
 		super();
@@ -178,6 +193,22 @@ public class Utilisateur {
 	}
 	
 	
+
+	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse) {
+		super();
+		this.noUtilisateur = noUtilisateur;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+	
+	}
 
 	public Utilisateur() {
 		super();
