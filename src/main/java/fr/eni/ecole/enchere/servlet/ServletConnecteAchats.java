ded4ea005463c92933f1501fb3e602bc07ec5dbc
twillem2022 <twillem2022@16SE44-HRH3P02.ad.campus-eni.fr>
2023-02-02 14:45:57 +0100

    TW CHANGE serlvet userconnecte -> userconnecteAchats

--- src/main/java/fr/eni/ecole/enchere/servlet/ServletConnecteAchats.java
+++ src/main/java/fr/eni/ecole/enchere/servlet/ServletConnecteAchats.java
@@ -11,7 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.ecole.enchere.bll.EnchereManager;
 import fr.eni.ecole.enchere.bo.Enchere;
+import fr.eni.ecole.enchere.exception.BusinessException;
 
 /**
  * Servlet implementation class ServletConnexion
@@ -20,7 +22,7 @@ import fr.eni.ecole.enchere.bo.Enchere;
 public class ServletConnecteAchats extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	private List<Enchere> listeEncheres = new ArrayList<>();
+	
 	
 	
 	
@@ -34,6 +36,23 @@ public class ServletConnecteAchats extends HttpServlet {
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 				
+		EnchereManager mgr = new EnchereManager();
+
+		try {
+			List<Enchere> listeEncheres = mgr.afficherEncheres();
+			request.setAttribute("listeEncheres", listeEncheres);
+			for(Enchere e : listeEncheres) {
+				System.out.println("Servlet encheres : " + e.getMontantEnchere());
+			}
+		} catch (BusinessException e) {
+			e.printStackTrace();
+			BusinessException be = new BusinessException();
+			be.addMessage("ca marche pas");
+		}
+		
+		
+		
+		
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/userConnecteAchats.jsp");
 		rd.forward(request, response);
