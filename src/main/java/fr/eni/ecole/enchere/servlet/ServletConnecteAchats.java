0fa6f92650454e3325bbb5bb0063733fb006169c
twillem2022 <twillem2022@16SE44-HRH3P02.ad.campus-eni.fr>
2023-02-02 14:45:29 +0100

    TW ADD methodes mes encheres DAO

--- src/main/java/fr/eni/ecole/enchere/servlet/ServletConnecteAchats.java
+++ src/main/java/fr/eni/ecole/enchere/servlet/ServletConnecteAchats.java
@@ -1,6 +1,8 @@
 package fr.eni.ecole.enchere.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,13 +11,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.ecole.enchere.bo.Enchere;
+
 /**
  * Servlet implementation class ServletConnexion
  */
 @WebServlet("/ServletConnecte")
-public class ServletConnecte extends HttpServlet {
+public class ServletConnecteAchats extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private List<Enchere> listeEncheres = new ArrayList<>();
+	
+	
+	
+	
+	
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
@@ -23,8 +33,9 @@ public class ServletConnecte extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/userConnecte.jsp");
+				
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/userConnecteAchats.jsp");
 		rd.forward(request, response);
 	}
 
