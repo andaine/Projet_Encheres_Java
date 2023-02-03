293463f07250b09715f94350d10565e1c54f50ac
gbaudouin2022 <gbaudouin2022@16SE44-HQH3P02.ad.campus-eni.fr>
2023-02-03 17:32:47 +0100

    GB nouvelle vente

--- src/main/java/fr/eni/ecole/enchere/dal/ArticleDAO.java
+++ src/main/java/fr/eni/ecole/enchere/dal/ArticleDAO.java
@@ -2,12 +2,15 @@ package fr.eni.ecole.enchere.dal;
 
 import java.util.List;
 
+import fr.eni.ecole.enchere.bo.Article;
 import fr.eni.ecole.enchere.bo.Categorie;
 import fr.eni.ecole.enchere.exception.BusinessException;
 
 public interface ArticleDAO {
 
 	public List<Categorie>selectCategories() throws BusinessException;
+
+	public void ajouterVente(Article article) throws BusinessException;
 	
 	
 }
