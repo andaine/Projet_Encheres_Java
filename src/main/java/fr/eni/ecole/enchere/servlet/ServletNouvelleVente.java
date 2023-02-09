47d72b4e15c5c497b54bf236b93cba8725f859eb
twillem2022 <twillem2022@16SE44-HRH3P02.ad.campus-eni.fr>
2023-02-09 16:30:16 +0100

    tw clean servlet nouvelle vente

--- src/main/java/fr/eni/ecole/enchere/servlet/ServletNouvelleVente.java
+++ src/main/java/fr/eni/ecole/enchere/servlet/ServletNouvelleVente.java
@@ -2,11 +2,7 @@ package fr.eni.ecole.enchere.servlet;
 
 import java.io.IOException;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -31,88 +27,84 @@ import fr.eni.ecole.enchere.exception.BusinessException;
 public class ServletNouvelleVente extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		request.setCharacterEncoding("UTF-8");
 
 		ArticleManager am = ArticleManager.getInstance();
 
 		try {
 			List<Categorie> listeCategories = am.afficherCategories();
-
 			request.setAttribute("categorie", listeCategories);
 		} catch (BusinessException e) {
 			e.printStackTrace();
 		}
-		
+
 		HttpSession session = request.getSession();
 		Utilisateur userConnecte = (Utilisateur) session.getAttribute("userConnecte");
 		if (request.getAttribute("rue") == null) {
 			request.setAttribute("rue", userConnecte.getRue());
-			request.setAttribute("postal",userConnecte.getCodePostal());
-			request.setAttribute("ville",userConnecte.getVille());
-		
+			request.setAttribute("postal", userConnecte.getCodePostal());
+			request.setAttribute("ville", userConnecte.getVille());
+
 		}
-		
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/nouvelleVente.jsp");
 		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
+		// recupérer les textfield
 		request.setCharacterEncoding("UTF-8");
 		String article = request.getParameter("article");
 		String description = request.getParameter("description");
-		int noCategorie = Integer.parseInt(request.getParameter("categorie")) ;
-		int prix  = Integer.parseInt(request.getParameter("prix")) ;
+		int noCategorie = Integer.parseInt(request.getParameter("categorie"));
+		int prix = Integer.parseInt(request.getParameter("prix"));
 		LocalDateTime debutVente = LocalDateTime.parse(request.getParameter("debut"));
 		LocalDateTime finVente = LocalDateTime.parse(request.getParameter("fin"));
-		System.out.println(request.getParameter("debut"));
-		
 		String rue = request.getParameter("rue");
-	
 		String codePostal = request.getParameter("postal");
 		String ville = request.getParameter("ville");
+
+		// set l'attribut catégorie pour la réafficher
 		Categorie cat = new Categorie(noCategorie, null);
 		request.setAttribute("categorieChoisie", noCategorie);
+
 		ArticleManager am = ArticleManager.getInstance();
-		
+
 		try {
-			
+
 			Retrait retraitVendeur = new Retrait(rue, codePostal, ville);
-			
-			Article art = new Article(article, description, debutVente, finVente, prix, prix, cat, "CR", retraitVendeur);
-			System.out.println(art);
-			System.out.println(retraitVendeur);
+			Article art = new Article(article, description, debutVente, finVente, prix, prix, cat, "CR",
+					retraitVendeur);
+
 			HttpSession session = request.getSession();
 			Utilisateur userConnecte = (Utilisateur) session.getAttribute("userConnecte");
 			int id = userConnecte.getNoUtilisateur();
-			
-			
-			
 			am.ajouterVente(art, id);
-		} catch (BusinessException e) {
-			request.setAttribute("article",article);
-			request.setAttribute("description",description);
 
-			request.setAttribute("prix",prix);
-			request.setAttribute("debut",debutVente);
-			request.setAttribute("fin",finVente);
+		} catch (BusinessException e) {
+			request.setAttribute("article", article);
+			request.setAttribute("description", description);
+			request.setAttribute("prix", prix);
+			request.setAttribute("debut", debutVente);
+			request.setAttribute("fin", finVente);
 			request.setAttribute("rue", rue);
-			request.setAttribute("postal",codePostal);
-			request.setAttribute("ville",ville);
+			request.setAttribute("postal", codePostal);
+			request.setAttribute("ville", ville);
 			request.setAttribute("listeErreur", e.getListeMessage());
 			doGet(request, response);
-		
-		} 
+		}
 
 		RequestDispatcher rd = request.getRequestDispatcher("/ServletAccueil");
 		rd.forward(request, response);
 	}
 
-	
 }
